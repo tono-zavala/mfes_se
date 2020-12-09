14b2f5d4e3c56087713fed04060de8eee02a053c
Ricardo García <rjozze10@gmail.com>
2020-12-09 04:36:04 -0600

    instalacion del frontend proyecto fe_mfes, realizacion de los componentes comunes page, field, buttons, headder, footer privados Listas de productos, Publicos Home y login, rutas al app

--- fe_mfes/src/cmps/cmns/Header.js
+++ fe_mfes/src/cmps/cmns/Header.js
@@ -1,9 +1,12 @@
-import './Header.css';
-const Header = ({children}) =>{
-    return (
+import "./Header.css";
+
+const Header = ( {children} )=>{
+    return(
         <header className="header">
             <h1>{children}</h1>
         </header>
-    );
+    )
 }
+
+//Read about css namespacing
 export default Header;
