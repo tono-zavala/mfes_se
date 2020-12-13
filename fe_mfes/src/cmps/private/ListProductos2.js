cd65156441b0e2d50a8e97bdc2b0c782e0698f30
tono-zavala <tono-zavala@users.noreply.github.com>
2020-12-12 23:47:57 -0600

    Subdocumentos actualizado

--- fe_mfes/src/cmps/private/ListProductos2.js
+++ fe_mfes/src/cmps/private/ListProductos2.js
@@ -1,33 +1,56 @@
 import Page from '../cmns/Page';
 import { AddButton } from '../cmns/Buttons';
 import './ListProductos.css';
+import { useEffect, useState } from 'react';
+import { useStateContext } from '../../utlts/Context';
+import {paxios} from '../../utlts/Axios';
 
-const dummyData = [
-    {"_id": 1,"label":"contenido 1","count":1},
-    {"_id": 2,"label":"contenido 2","count":2},
-    {"_id": 3,"label":"contenido 3","count":3},
-    {"_id": 4,"label":"contenido 4","count":4},
-    {"_id": 5,"label":"contenido 5","count":5},
-    {"_id": 6,"label":"contenido 6","count":6},
-    {"_id": 7,"label":"contenido 7","count":7},
-    {"_id": 8,"label":"contenido 8","count":8},
-    {"_id": 9,"label":"contenido 9","count":9},
-    {"_id": 10,"label":"contenido 10","count":10},
-    {"_id": 11,"label":"contenido 11","count":11},
-    {"_id": 12,"label":"contenido 12","count":12},
-    {"_id": 13,"label":"contenido 13","count":13},
-    {"_id": 14,"label":"contenido 14","count":14},
-    {"_id": 15,"label":"contenido 15","count":15},
-    {"_id": 16,"label":"contenido 16","count":16},
-];
+import {PRODUCT_LOADED, PRODUCT_LOADING} from '../../utlts/store/reducers/prods.reducer';
 
-const ListProductos2 = () =>{
+// const dummyData = [
+//     {"_id": 1,"label":"contenido 1","count":1},
+//     {"_id": 2,"label":"contenido 2","count":2},
+//     {"_id": 3,"label":"contenido 3","count":3},
+//     {"_id": 4,"label":"contenido 4","count":4},
+//     {"_id": 5,"label":"contenido 5","count":5},
+//     {"_id": 6,"label":"contenido 6","count":6},
+//     {"_id": 7,"label":"contenido 7","count":7},
+//     {"_id": 8,"label":"contenido 8","count":8},
+//     {"_id": 9,"label":"contenido 9","count":9},
+//     {"_id": 10,"label":"contenido 10","count":10},
+//     {"_id": 11,"label":"contenido 11","count":11},
+//     {"_id": 12,"label":"contenido 12","count":12},
+//     {"_id": 13,"label":"contenido 13","count":13},
+//     {"_id": 14,"label":"contenido 14","count":14},
+//     {"_id": 15,"label":"contenido 15","count":15},
+//     {"_id": 16,"label":"contenido 16","count":16},
+// ];
 
-    const listElements = dummyData.map((o)=>{
-        return (<li key={o._id}>{o.label} <span>{o.count}</span></li>);
+const ListProductos = () =>{
+    const[{prods}, dispatch]= useStateContext();
+
+    const listElements =  prods.productos.map((o)=>{
+        let subdocument = o.colores || [{"color": "No disponible", "ide": Math.random()}];
+        // let myData = this.props.myData || {}
+        // console.log(subdocument);
+        return (<li key={o._id}>{o.nombre} {subdocument.map((ob)=><span key={ob.ide}>{ob.color}</span>)}</li>);
     })
+    ;
+    useEffect(
+        ()=>{
+            dispatch({type: PRODUCT_LOADING})
+            paxios.get('/api/productos/jeans')
+            .then(({data})=>{
+                dispatch({type:PRODUCT_LOADED, payload:data});
+                console.log(data);
+            })
+            .catch((ex)=>{
+                console.log(ex);
+            });
+        }
+    ,[]);
     return (
-        <Page headding="Pantalones" footer={true}>
+        <Page headding="Blusas" footer={true}>
             <ul className="productoList">
                 {listElements}
             </ul>
@@ -35,4 +58,4 @@ const ListProductos2 = () =>{
         </Page>
     );
 }
-export default ListProductos2;
+export default ListProductos;
--- fe_mfes/src/utlts/store/reducers/prods.reducer.js
+++ fe_mfes/src/utlts/store/reducers/prods.reducer.js
@@ -1,6 +1,6 @@
 const initialState = {
     hasMore:true,
-    products:[],
+    productos:[],
     total:0,
     currentPage:0,
     pageLimit:10,
@@ -9,20 +9,20 @@ const initialState = {
     currentId:null
   }
   
-  export const PRODUCT_LOADING = "";
-  export const PRODUCT_LOADED = "";
-  export const PRODUCT_ERROR = "";
-  export const PRODUCT_SET_CURRENT = "";
+  export const PRODUCT_LOADING = "PRODUCT_LOADING";
+  export const PRODUCT_LOADED = "PRODUCT_LOADED";
+  export const PRODUCT_ERROR = "PRODUCT_ERROR";
+  export const PRODUCT_SET_CURRENT = "PRODUCT_SET_CURRENT";
   
   
   const prodsReducer = (state = initialState, action = {}) =>{
     switch(action.type){
       case PRODUCT_LOADING:
-        return {...state};
+        return {...state, fetching:true};
       case PRODUCT_LOADED:
-        return {...state}
+        return {...state, productos:[...state.productos, ...action.payload], fetching:false}
       case PRODUCT_ERROR:
-        return {...state}
+        return {...state, fetching:false}
       case PRODUCT_SET_CURRENT:
         return {...state}
       default:
